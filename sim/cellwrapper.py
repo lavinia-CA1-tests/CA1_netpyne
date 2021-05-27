4bcf1a621bb0e1c4db7367a2369676bfd71aa09a
fernando <fernandodasilvaborges@gmail.com>
2021-05-27 03:00:39 -0300

    bbp-data-imported

--- sim/cellwrapper.py
+++ sim/cellwrapper.py
@@ -1,13 +1,13 @@
 from neuron import h
 
-def loadCell(cellName, MorphoName):
+def loadCell(hocName, MorphoName):
 
     h.load_file('import3d.hoc')
     h.load_file('stdrun.hoc')
     MorphologyPath = '/home/fernando/CA1_netpyne/info/data-bbp/20191017/morphologies/swc/'
     gid = 1
-    h.load_file('cells/hoc/' + cellName + '.hoc')
+    h.load_file('cells/hoc/' + hocName + '.hoc')
 
-    cell = getattr(h, cellName)(gid,MorphologyPath,MorphoName)
+    cell = getattr(h,  hocName)(gid,MorphologyPath,MorphoName)
 
     return cell
