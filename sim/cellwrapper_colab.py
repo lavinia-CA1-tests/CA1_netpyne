80a21a03a2324b0046e095e038fa2aa790368be0
laviniamitiko <72054479+laviniamitiko@users.noreply.github.com>
2021-09-25 00:16:46 -0300

    Update cellwrapper_colab.py

--- sim/cellwrapper_colab.py
+++ sim/cellwrapper_colab.py
@@ -4,7 +4,7 @@ def loadCell(hocName, MorphoName):
 
     h.load_file('import3d.hoc')
     h.load_file('stdrun.hoc')
-    MorphologyPath = 'CA1_netpyne/info/data-bbp/20191017/morphologies/swc/'
+    MorphologyPath = '/home/jovyan/work/NetPyNE-UI/CA1_netpyne/info/data-bbp/20191017/morphologies/swc/'
     gid = 1
     h.load_file('cells/hoc/' + hocName + '.hoc')
 
